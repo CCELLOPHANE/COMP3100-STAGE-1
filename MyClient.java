43ad12dc31ccf3c8c7a9bf63472beb24772b6fc8
CCELLOPHANE <caeyaru@gmail.com>
2023-05-04 09:45:28 +1000

    .

--- MyClient.java
+++ MyClient.java
@@ -6,168 +6,140 @@ public class MyClient {
         String hostName = "localhost";
         int portNumber = 50000;
 
-        // Create a socket to make the connection to the server
-        Socket socket = new Socket(hostName, portNumber);
-        System.out.println("Connected to server: " + socket.getInetAddress().getHostName());
+        try {
+            // Create a socket to make the connection to the server
+            Socket socket = new Socket(hostName, portNumber);
+            System.out.println("Connected to server: " + socket.getInetAddress().getHostName());
 
-        OutputStream out = socket.getOutputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
-        // HELO
-        String message = "HELO\n";
-        byte[] bytes = message.getBytes();
-        out.write(bytes);
-        System.out.println("Sent message to server: " + message);
-        out.flush();
+            // HELO from my week 2 workshop
+            String message = "HELO\n";
+            byte[] bytes = message.getBytes();
+            out.write(bytes);
+            System.out.println("Sent message to server: " + message);
 
-        // Send AUTH username - code from the week 4 workshop
-        String username = System.getProperty("user.name");
-        String authMessage = "AUTH " + username + "\n";
-        byte[] authBytes = authMessage.getBytes();
-        out.write(authBytes);
-        System.out.println("Sent authentication message to server: " + authMessage);
+            // Recieve OK
+            String response = reader.readLine();
+            System.out.println("Received message from server: " + response);
 
-        // Recieve OK
-        String response = reader.readLine();
-        System.out.println("Received message from server: " + response);
-        out.flush();
+            // Send AUTH username - code from the week 4 workshop
+            String username = System.getProperty("user.name");
+            String authMessage = "AUTH " + username + "\n";
+            byte[] authBytes = authMessage.getBytes();
+            out.write(authBytes);
+            System.out.println("Sent authentication message to server: " + authMessage);
 
-        String r = "";
-        // Send REDY
-        message = "REDY\n";
-        bytes = message.getBytes();
-        out.write(bytes);
-        System.out.println("Sent message to server: " + message);
-
-        // Receive a message JOBN, JCPL and NONE
-        response = reader.readLine();
-        r = response;
-
-        int jobID = 0;
-        if (response.contains("JOBN")) {
-            String[] jobCom = response.split(" ");
-            jobID = Integer.parseInt(jobCom[2]);
-        }
-
-        System.out.println("Received message from server: " + response);
-        out.flush();
-
-        // Send a GETS message
-        message = "GETS All \n";
-        bytes = message.getBytes();
-        out.write(bytes);
-        System.out.println("Sent message to server: " + message);
-
-        response = reader.readLine();
-        System.out.println("Received message from server: " + response);
-        out.flush();
-
-        // Receive DATA nRecs recSize // e.g., DATA 5 124
-        int nRecs = 0;
-        if ((response.contains("DATA"))) {
-            String[] data = response.split("\\s+");
-            nRecs = Integer.parseInt(data[1]);
-        }
-
-        // Send OK
-        message = "OK\n";
-        bytes = message.getBytes();
-        out.write(bytes);
-        System.out.println("Sent message to server: " + message);
-
-        // For loop to store records
-        for (int i = 0; i < nRecs; i++) {
-            // Receive a record
+            // Recieve OK
             response = reader.readLine();
             System.out.println("Received message from server: " + response);
-            // Keep track of the number of records received
-            i++;
-        }
 
-        message = "OK\n";
-        bytes = message.getBytes();
-        out.write(bytes);
-        System.out.println("Sent message to server: " + message);
-
-        String serverType = "";
-        int serverID = 0;
-        int serverCore = 0;
-
-        // Find the server with the most cores
-        for (int i = 0; i < nRecs; i++) {
-            response = reader.readLine();
-            String[] responseType = response.split(" ");
-            if (serverCore < Integer.parseInt(responseType[4])) {
-                serverType = responseType[0];
-                serverCore = Integer.parseInt(responseType[4]);
-                serverID = Integer.parseInt(responseType[1]);
-            }
-            if (serverType.equals(responseType[0])) {
-                serverID = Integer.parseInt(responseType[1]);
-            }
-            System.out.println("Received message from server: " + response);
-        }
-
-        // Send OK
-        message = "OK\n";
-        bytes = message.getBytes();
-        out.write(bytes);
-        System.out.println("Sent message to server: " + message);
-
-        // Recieve
-        response = reader.readLine();
-        System.out.println("Received message from server: " + response);
-        out.flush();
-        int ID = 0;
-
-        while (!responseS.contains("NONE")) {
-            // If the response is jobn n keep scheluling untill we reach NONE
-            if (responseS.contains("JOBN")) {
-                if (ID > serverID) {
-                    ID = 0;
-                }
-                message = "SCHD " + jobID + " " + serverType + " " + ID + "\n";
+            response = "";
+            while (!response.contains("NONE")) {
+                // Send REDY
+                message = "REDY\n";
                 bytes = message.getBytes();
                 out.write(bytes);
                 System.out.println("Sent message to server: " + message);
+
+                // Receive a message JOBN, JCPL and NONE
                 response = reader.readLine();
                 System.out.println("Received message from server: " + response);
-                out.flush();
-                ID++;
 
+                // Send a GETS message
+                message = "GETS All \n";
+                bytes = message.getBytes();
+                out.write(bytes);
+                System.out.println("Sent message to server: " + message);
+
+                // Receive DATA nRecs recSize // e.g., DATA 5 124
+                int nRecs = 0;
+                if ((response.contains("DATA"))) {
+                    String[] data = response.split("\\s+");
+                    nRecs = Integer.parseInt(data[1]);
+                }
+
+                // identify largest server type
+                // SCHD
+                String[] jobData = response.split("\\s+");
+                String jobID = jobData[2];
+                int estRuntime = Integer.parseInt(jobData[3]);
+                int coreCount = Integer.parseInt(jobData[4]);
+                int memory = Integer.parseInt(jobData[5]);
+                int disk = Integer.parseInt(jobData[6]);
+                int c = 0;
+                if (coreCount > c) {
+                    c = coreCount;
+                }
+                if (memory > c) {
+                    c = memory;
+                }
+                if (disk > c) {
+                    c = disk;
+                }
+                if (estRuntime > c) {
+                    c = estRuntime;
+                }
+
+                // Send OK
+                message = "OK\n";
+                bytes = message.getBytes();
+                out.write(bytes);
+                System.out.println("Sent message to server: " + message);
+
+                // For loop to store records
+                for (int i = 0; i < nRecs; i++) {
+                    // Receive a record
+                    response = reader.readLine();
+                    System.out.println("Received message from server: " + response);
+                    // keep track of the number of records received
+                    i++;
+                }
+
+                // Send OK
+                message = "OK\n";
+                bytes = message.getBytes();
+                out.write(bytes);
+                System.out.println("Sent message to server: " + message);
+
+                // Recieve
+                response = reader.readLine();
+                System.out.println("Received message from server: " + response);
+
+                // If the message received at Step 10 is JOBN then schedule job
+                if (response.contains("JOBN")) {
+                    int server = c;
+                    String schdMessage = "SCHD " + jobID + " " + server + "\n";
+                    byte[] schdBytes = schdMessage.getBytes();
+                    out.write(schdBytes);
+                    System.out.println("Sent scheduling message to server: " + schdMessage);
+
+                    message = "SCHD " + jobID + " " + server + "\n";
+                    bytes = message.getBytes();
+                    out.write(bytes);
+                    System.out.println("Sent scheduling message to server: " + schdMessage);
+                }
             }
-            // If the response is JCPL n keep scheluling until we reach NONE
-            message = "REDY\n";
+
+            // Send QUIT
+            message = "QUIT\n";
             bytes = message.getBytes();
             out.write(bytes);
             System.out.println("Sent message to server: " + message);
 
+            // Recieve QUIT
             response = reader.readLine();
-            r = response;
-
-            // Saving job ID
-            if (r.contains("JOBN")) {
-                String[] jobCom = r.split(" ");
-                jobID = Integer.parseInt(jobCom[2]);
-            }
-
             System.out.println("Received message from server: " + response);
-            out.flush();
+
+            // Close the socket
+            socket.close();
+
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
         }
 
-        // Send QUIT
-        message = "QUIT\n";
-        bytes = message.getBytes();
-        out.write(bytes);
-        System.out.println("Sent message to server: " + message);
-
-        // Recieve QUIT
-        response = reader.readLine();
-        System.out.println("Received message from server: " + response);
-
-        // Close the socket
-        out.close();
-        reader.close();
-        socket.close();
     }
+
 }
